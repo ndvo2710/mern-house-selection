28e629b86d008336719dc05110e3741c7d94d8f4
ndv0 <ndv0@pop-os.localdomain>
2021-01-31 04:30:10 -0800

    clean up

--- backend/controllers/zillowPageController.js
+++ backend/controllers/zillowPageController.js
@@ -26,26 +26,3 @@ class zillowController {
 }
 
 export default zillowController;
-
-
-// export const createNewZillowPage = async (req, res) => {
-//     logger.info('Calling /zillow-page');
-//     logger.info('create new ds');
-//     logger.debug(`req.body: ${JSON.stringify(req.body)}`);
-//     logger.debug(`zillowPageDict: ${JSON.stringify(req.body.zillowPageDict)}`);
-//     try {
-//         const newZillowPage = await dsService.addNew(req.body.zillowPageDict);
-//         logger.info(`newZillowPage: ${newZillowPage}`);
-//         return res.status(200).send({
-//             success: newZillowPage
-//         });
-//     } catch (error) {
-//         logger.debug(error);
-//         logger.debug(`errorMessage: ${error.message}`);
-//         return res.status(500).send({
-//             error: error.message
-//         });
-//     }
-// }
-
-
