0b4d3dfa5f1da9a03189e49e9981e5f9f905d922
ndv0 <ndv0@pop-os.localdomain>
2021-02-01 03:49:25 -0800

    Add is_chosen field in ds DB

--- backend/models/dsModel.js
+++ backend/models/dsModel.js
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose';
 
 const dsSchema = new mongoose.Schema({
-    ds: { type: String, required: true },
+    ds: { type: String, trim: true, required: true },
+    isChosen: { type: Boolean, default: true },
     createdAt: { type: Number, default: Date.now },
     updatedAt: { type: Number, default: null },
     deletedAt: { type: Number, default: null },
 });
 
 dsSchema.statics = {
-    createNew(item) {
-        return this.create({ ds: item });
+    createNew(currentDs) {
+        return this.create({ ds: currentDs, isChosen: true });
     },
     checkExists(currentDs) {
         return this.findOne({ ds: currentDs }).exec();
