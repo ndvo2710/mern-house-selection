339a07d56c9d0004fcf3fae910fe39b2c47f39bc
ndv0 <ndv0@pop-os.localdomain>
2021-02-07 14:18:10 -0800

    click new section button

--- client/src/App.js
+++ client/src/App.js
@@ -3,34 +3,45 @@ import Houses from './components/Houses';
 import mockData from './mockData';
 
 const HOUSE_API_URL = 'http://localhost:5000/fetch-zillow/02-01-2021';
+const DS_API_URL = 'http://localhost:5000/fetch-ds';
 
 
 function App() {
-    const [houseList, setHouseList] = useState(mockData);
+  const [houseList, setHouseList] = useState(mockData);
+  const [dsList, setDsList] = useState([]);
 
-    useEffect(() => {
-        async function fetchHouses() {
-            const res = await fetch(HOUSE_API_URL);
-            const data = await res.json();
-            console.log('Testing');
-            console.log(data.success);
-            setHouseList(data.success);
-        }
+  useEffect(() => {
+    async function fetchHouses() {
+      const res = await fetch(HOUSE_API_URL);
+      const data = await res.json();
+      console.log('Testing');
+      console.log(data.success);
+      setHouseList(data.success);
+    }
+    async function fetchDS() {
+      const res = await fetch(DS_API_URL);
+      const data = await res.json();
+      console.log('Testing');
+      console.log(data.success);
+      setDsList(data.success);
+    }
 
-        (fetchHouses)();
+    (fetchHouses)();
+    (fetchDS)();
 
-        return () => {
-            // cleanup
-        }
-    }, [
-        // input
-    ])
+    return () => {
+      // cleanup
+    }
+  }, [
+    // input
+  ])
 
-    return (
-        <div className="app">
-            <Houses houses={houseList} />
-        </div>
-    );
+
+  return (
+    <div className="app">
+      <Houses houses={houseList} dsArray={dsList} setDsList={setDsList} />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/Houses/index.jsx
+++ client/src/components/Houses/index.jsx
@@ -1,22 +1,57 @@
+// import React from 'react';
 import React, { useState } from 'react';
-import { Button, Col, Container, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Input, Row } from "reactstrap";
+import { Button, Col, Container, Input, Row } from "reactstrap";
+// import useDropdown from '../../hooks/useDropDown';
 import HouseRow from '../HouseRow';
 
+const DS_API_URL = 'http://localhost:5000/fetch-ds';
 
 
-function Houses({ houses }) {
-    const [dropdownOpen, setDropdownOpen] = useState(false);
 
-    const toggle = () => setDropdownOpen(prevState => !prevState);
+function Houses({ houses, dsArray, setDsList }) {
 
 
-    console.log(houses);
     const numHousesOnRow = 3;
     const houseArrays = [];
     for (let i = 0; i < houses.length; i += numHousesOnRow) {
         houseArrays.push(houses.slice(i, i + numHousesOnRow));
     }
-    console.log(houseArrays);
+    console.log('houseArrays: ', houseArrays);
+    console.log('dsArray: ', dsArray);
+
+    // const [dsState, dsDropdown, setDsState] = useDropdown('dsDropdown', "", dsArray);
+    const [dropDownValue, setDropDownValue] = useState("")
+
+    async function newSectionOnClickHandle(setDropDownValue) {
+        let today = new Date().toISOString().slice(0, 10);
+        const url = 'http://localhost:5000/create-ds'
+
+        const requestConfig = {
+            method: 'POST', // *GET, POST, PUT/PATCH, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *client
+            body: JSON.stringify({ currentDs: today }) // body data type must match "Content-Type" header
+        };
+        const response = await fetch(url, requestConfig);
+        const res = await response.json();
+        if ('success' in res) {
+            const res = await fetch(DS_API_URL);
+            const data = await res.json();
+            console.log('Testing');
+            console.log(data.success);
+            setDsList(data.success);
+            setDropDownValue(today);
+        } else {
+            console.log(res);
+        }
+    }
+
 
 
     return (
@@ -27,12 +62,6 @@ function Houses({ houses }) {
                         <Col lg="8" xs="12">
                             <h1 className="display-3">
                                 MERN House Selection Project
-                                {/* <Col className="pl-lg-0" sm="4" xs="12">
-                                    <Button block color="primary" type="submit">
-                                        New Section
-                                        </Button>
-
-                                </Col> */}
                             </h1>
                             <Row className="row-input">
                                 <Col sm="4" xs="12">
@@ -44,34 +73,37 @@ function Houses({ houses }) {
                                         type="text"
                                     ></Input>
                                 </Col>
-                                <Col className="pl-lg-0" sm="2" xs="9">
+                                {/* <Col className="pl-lg-0" sm="2" xs="9">
                                     <Button block color="primary" type="submit">
                                         Add
-                                        </Button>
+                                    </Button>
 
+                                </Col> */}
+                                <Col className="pl-lg-0" sm="2" xs="9">
+                                    <label htmlFor={"dsDropdown"}>
+                                        {"Section start from:   "}
+
+                                        <select
+                                            id={"dsDropdown"}
+                                            value={dropDownValue}
+                                            onChange={e => setDropDownValue(e.target.value)}
+                                            onBlur={e => setDropDownValue(e.target.value)}
+                                            disabled={!dsArray.length}
+                                        >
+                                            {dsArray.map(dsItem =>
+                                                <option value={dsItem.ds}>{dsItem.ds}</option>)}
+                                        </select>
+                                    </label>
                                 </Col>
                                 <Col className="pl-lg-0" sm="2" xs="9">
-                                    <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-                                        <DropdownToggle caret>
-                                            Dropdown
-                                    </DropdownToggle>
-                                        <DropdownMenu>
-                                            <DropdownItem header>Header</DropdownItem>
-                                            <DropdownItem>Some Action</DropdownItem>
-                                            <DropdownItem text>Dropdown Item Text</DropdownItem>
-                                            <DropdownItem disabled>Action (disabled)</DropdownItem>
-                                            <DropdownItem divider />
-                                            <DropdownItem>Foo Action</DropdownItem>
-                                            <DropdownItem>Bar Action</DropdownItem>
-                                            <DropdownItem>Quo Action</DropdownItem>
-                                        </DropdownMenu>
-                                    </Dropdown>
-
-                                </Col>
-                                <Col className="pl-lg-0" sm="2" xs="9">
-                                    <Button block color="primary" type="submit">
+                                    <Button
+                                        block color="primary"
+                                        type="submit"
+                                        className="new-section"
+                                        onClick={e => newSectionOnClickHandle(setDropDownValue)}
+                                    >
                                         New Section
-                                        </Button>
+                                    </Button>
 
                                 </Col>
                             </Row>
